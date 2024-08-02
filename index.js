3d23372c0bb9ff583d179a657cd7eab8088214d8
kolamat <matanmidupe004@gmail.com>
2024-08-02 17:40:05 +0100

    with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   Bcake.jpg
    	new file:   Bcake2.jpeg
    	new file:   Bcake3.webp
    	new file:   Bcake4.jpg
    	new file:   index.html
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const heading = document.createElement("h1");
+heading.textContent = "Happy Birthday Micheal";
+
+const greetingText =
+  "How are you doing Micheal hope you Good Sorry it cake late";
+
+const image1 = document.createElement("img");
+const image2 = document.createElement("img");
+const image3 = document.createElement("img");
+const image4 = document.createElement("img");
