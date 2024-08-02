405cd65472e015bc0e2156d274da8ba072758bb9
kolamat <matanmidupe004@gmail.com>
2024-08-02 19:33:45 +0100

    On branch main Your branch is ahead of 'origin/main' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const body = document.querySelector("body");
-// body.style.background = "brown";
 body.style.margin = "0px";
 body.style.padding = "0px";
-
-const heading = document.createElement("h1");
-heading.textContent = "Happy Birthday Micheal";
 
 const greetingText =
   "How are you doing Micheal hope you Good Sorry it cake late";
@@ -15,34 +11,48 @@ div.style.width = "full";
 div.style.display = "grid";
 div.style.justifyItems = "center";
 div.style.gridTemplateColumns = "repeat(4, minmax(0, 1fr))";
-// div.textContent = "Happy birthday";
 div.style.background = "#a15f5f";
 document.querySelector("body").appendChild(div);
 
+const heading = document.createElement("h2");
+heading.textContent = "Happy Birthday Micheal";
+heading.style.gridColumn = "span 4";
+document.getElementById("container").appendChild(heading);
+
 const image1 = document.createElement("img");
 image1.src = "Bcake.jpg";
-image1.style.borderRadius = "15px 20px";
-image1.style.gridColumn = "span 4 / span 4";
+image1.style.borderRadius = "10px 10px";
+image1.style.gridColumn = "span 4";
 image1.style.width = "100%";
 document.getElementById("container").appendChild(image1);
 
 const image2 = document.createElement("img");
 image2.src = "Bcake2.jpeg";
 image2.style.width = "100%";
-image2.style.gridColumn = "span 2 / span 2";
-image2.style.borderRadius = "15px 20px";
+image2.style.width = "100%";
+image2.style.gridColumn = "span 2";
+image2.style.borderRadius = "10px 10px";
 document.getElementById("container").appendChild(image2);
 
 const image3 = document.createElement("img");
 image3.src = "Bcake3.webp";
 image3.style.width = "100%";
-image3.style.borderRadius = "15px 20px";
-image3.style.gridColumn = "span 2 / span 2";
+image3.style.height = "100%";
+image3.style.borderRadius = "10px 10px";
+image3.style.gridColumn = "span 2";
 document.getElementById("container").appendChild(image3);
 
 const image4 = document.createElement("img");
 image4.src = "Bcake4.jpg";
 image4.style.width = "100%";
-image4.style.gridColumn = "span 4 / span 4";
-image4.style.borderRadius = "15px 20px";
+image4.style.gridColumn = "span 4";
+image4.style.borderRadius = "10px 10px";
 document.getElementById("container").appendChild(image4);
+
+const buttton = document.createElement("button");
+buttton.textContent = "Click Me for More";
+buttton.style.padding = "10px 8px";
+buttton.style.gridColumn = "span 4";
+buttton.style.borderRadius = "20px";
+buttton.style.borderRadius = "20px";
+document.getElementById("container").appendChild(buttton);
