30d3796474c33ab0dcf4ef39d6cbf19ba40d5cfc
kolamat <matanmidupe004@gmail.com>
2024-08-04 09:27:14 +0100

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   index.js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const body = document.querySelector("body");
 body.style.margin = "0px";
 body.style.padding = "0px";
 
-const greetingText =
-  "How are you doing Micheal hope you Good Sorry it cake late";
-
 const div = document.createElement("div");
 div.id = "container";
 div.style.width = "full";
@@ -21,7 +18,7 @@ document.getElementById("container").appendChild(heading);
 
 const image1 = document.createElement("img");
 image1.src = "Bcake.jpg";
-image1.style.borderRadius = "10px 10px";
+image1.style.borderRadius = "10px 10px 0px 0px";
 image1.style.gridColumn = "span 4";
 image1.style.width = "100%";
 document.getElementById("container").appendChild(image1);
@@ -46,13 +43,38 @@ const image4 = document.createElement("img");
 image4.src = "Bcake4.jpg";
 image4.style.width = "100%";
 image4.style.gridColumn = "span 4";
-image4.style.borderRadius = "10px 10px";
+image4.style.borderRadius = "0px 0px 10px 10px";
 document.getElementById("container").appendChild(image4);
+
+const paraDiv = document.createElement("p");
+paraDiv.id = "para";
+paraDiv.style.fontWeight = "bold";
+paraDiv.style.fontSize = "15px";
+paraDiv.style.color = "green";
+document.querySelector("container").appendChild(paraDiv);
+
+// const greetingText = document.createElement("p");
+// greetingText.textContent = document
+//   .getElementById("container")
+//   .appendChild(greetingText);
 
 const buttton = document.createElement("button");
 buttton.textContent = "Click Me for More";
 buttton.style.padding = "10px 8px";
+buttton.id = "buttons";
+buttton.onclick = greetingText;
 buttton.style.gridColumn = "span 4";
 buttton.style.borderRadius = "20px";
-buttton.style.borderRadius = "20px";
+buttton.style.margin = "20px 0px 10px 0px";
+buttton.style.borderColor = "white";
+buttton.style.background = "black";
+buttton.style.color = "white";
 document.getElementById("container").appendChild(buttton);
+
+function greetingText() {
+  let paraGraph = document.getElementById("para");
+  paraGraph.textContent =
+    "How are you doing Micheal hope you Good Sorry it came late";
+  paraGraph.id = "para";
+  document.querySelector("body").appendChild(paraGraph);
+}
